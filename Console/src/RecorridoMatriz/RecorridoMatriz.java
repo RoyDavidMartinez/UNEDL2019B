8a4dfe0a907d9e180d21ff3692585149328bc27c
Roy David Martinez Gomez <roydavid_98@hotmail.com>
2019-09-11 09:29:19 -0500

    RecorridoMatriz

---
+++ Console/src/RecorridoMatriz/RecorridoMatriz.java
@@ -0,0 +1,19 @@
+package RecorridoMatriz;
+
+public class RecorridoMatriz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int [][]myMatriz = {{1,3,5,7},{4,7,9,7},{2,6,8,0},{2,4,5,2}};
+		
+		public static void RecorridoN(int [][] myMatriz) {
+			for (int i = 0;i<myMatriz.length;i++) {
+				for(int j=0;j<myMatriz[i].length;j++) {
+					
+				}
+			}
+		}
+	}
+
+}
