dcefd59f05c6e62cf3e301bce5d5a59b0f514a05
Roy David Martinez Gomez <roydavid_98@hotmail.com>
2019-11-25 10:06:43 -0600

    Segundo parcial

---
+++ Console/Examen_2do_Parcial/Examen_2do_Parcial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+
+namespace Examen_2do_Parcial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack Checador = new Stack();
+            Checador.Push("{{}{}{{{}}");
+            Checador.Push("{{}{{{{}}}");
+            Checador.Push("{{{{}{}}}}");
+            Checador.Push("{{}{}}{{}}");
+
+            Checador.Pop();
+        }
+    }
+}
